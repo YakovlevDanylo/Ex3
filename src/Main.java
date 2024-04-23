4d683a237480c9ffdbb13ce61390be299de3d836
YakovlevDanylo <yakovlevdv31@gmail.com>
2024-04-23 23:25:37 +0300

    Create shapes, ArrayList of shapes and method that calculate sum of areas of all Shapes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<GeometricShape> shapes = new ArrayList<>();
+        shapes.add(new Circle(2.5));
+        shapes.add(new Circle(13));
+        shapes.add(new Square(5));
+        shapes.add(new Square(3));
+        shapes.add(new Triangle(2,5,3));
+        System.out.println(getAreaSum(shapes));
+    }
+
+    public static double getAreaSum(ArrayList<GeometricShape> shapes) {
+        double sum = 0;
+        for (GeometricShape shape : shapes) {
+            sum += shape.getArea();
+        }
+
+        return sum;
     }
 }
